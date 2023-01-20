7f5d23dae27bf316fb41466e6c4986ff089b8833
SebastianWTAMU <123107486+SebastianWTAMU@users.noreply.github.com>
2023-01-20 11:29:15 -0600

    Add files via upload
    Home 1 CS file

---
+++ Homework1.cs
@@ -0,0 +1,22 @@
+﻿/* 
+ Author: Sebastian Gonzalez
+ Homework 1
+*/
+
+
+
+namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+// The linear line Equation is: Z = 4X^2+3Y, when  X = 2.5, Y=3.3
+
+    double X = 2.5;
+    double Y = 3.3;
+    
+    Console.WriteLine("X = 2.5" + "," + " Y = 3.3 ");
+    Console.WriteLine( "The value of Z is: " + (4*X*X+3*Y));
+    
+    }
+}
